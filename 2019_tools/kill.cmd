af5277eb2a2590b30072fc1cc9a9ab3956e8bcbd
serg-bloim <serg.bloim@gmail.com>
2019-12-04 20:22:28 -0500

    Update kill.cmd

--- 2019_tools/kill.cmd
+++ 2019_tools/kill.cmd
@@ -1,7 +1,6 @@
 @echo
 cls
-taskkill /F /T /FI "IMAGENAME eq cmd.exe" /FI "WINDOWTITLE eq a2_"
-taskkill /IM  hat2.exe /T /F
+taskkill /F /T /FI "IMAGENAME eq cmd.exe" /FI "WINDOWTITLE eq a2_*"
+taskkill /F /T /IM  redhat.exe
 taskkill /F /T /FI "IMAGENAME eq a2serv*"
 exit
-
