da5686e0c02973a8a83b3b91afe94bd3ed1890ea
tangar <39904162+igroglaz@users.noreply.github.com>
2023-06-25 08:39:14 +0300

    Update _start.ps1

--- _configs/_start.ps1
+++ _configs/_start.ps1
@@ -2,7 +2,7 @@ $restart_hour = 03
 $restart_minute = 30
 $num_servers = 10
 
-while($true) {
+while ($true) {
     # Step 0: Kill existing processes
     Set-Location "C:\Allods2\"
     Start-Process -FilePath "cmd.exe" -ArgumentList "/c kill.cmd"
@@ -15,16 +15,21 @@ while($true) {
     # Step 2: Wait for the set time
     $current_hour = (Get-Date).Hour
     $current_minute = (Get-Date).Minute
-    $current_time = $current_hour*60 + $current_minute
-    $restart_time = $restart_hour*60 + $restart_minute
-    $waiting_time = ($restart_time - $current_time + 24*60) % (24*60)
+    $current_time = $current_hour * 60 + $current_minute
+    $restart_time = $restart_hour * 60 + $restart_minute
+    $waiting_time = ($restart_time - $current_time + 24 * 60) % (24 * 60)
     Start-Sleep -Seconds ($waiting_time * 60)
 
     # Step 3: Server restart notice
+    $command = {
+        param($server, $message)
+        Start-Process -FilePath "cmd.exe" -ArgumentList "/c", "msg.cmd", "$server", "`"$message`""
+    }
+
     for ($j = 30; $j -ge 11; $j -= 5) {
         1..$num_servers | ForEach-Object {
             $i = $_
-            Start-Process -FilePath "cmd.exe" -ArgumentList "/c", "msg.cmd", "$i", "`"Server will restart in $j minutes`""
+            Invoke-Command -ScriptBlock $command -ArgumentList $i, "Server will restart in $j minutes"
         }
         Start-Sleep -Seconds 300
     }
@@ -32,7 +37,7 @@ while($true) {
     for ($j = 10; $j -ge 2; $j -= 1) {
         1..$num_servers | ForEach-Object {
             $i = $_
-            Start-Process -FilePath "cmd.exe" -ArgumentList "/c", "msg.cmd", "$i", "`"Server will restart in $j minutes`""
+            Invoke-Command -ScriptBlock $command -ArgumentList $i, "Server will restart in $j minutes"
         }
         Start-Sleep -Seconds 60
     }
@@ -40,7 +45,7 @@ while($true) {
     for ($j = 60; $j -ge 1; $j -= 10) {
         1..$num_servers | ForEach-Object {
             $i = $_
-            Start-Process -FilePath "cmd.exe" -ArgumentList "/c", "msg.cmd", "$i", "`"Server will restart in $j seconds`""
+            Invoke-Command -ScriptBlock $command -ArgumentList $i, "Server will restart in $j seconds"
         }
         Start-Sleep -Seconds 10
     }
